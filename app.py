4cff1eb520b2efc0d9da7d99a5b66d5b6a1d865a
nhanbudle1-cmyk <nhanbudle1@gmail.com>
2026-03-25 21:57:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import requests
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+@app.route("/")
+def home():
+    return "VN30 BOT RUNNING"
+
+def send_signal(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": msg
+    }
+    requests.post(url, data=data)
+
+send_signal("✅ BOT VN30 đã hoạt động")
